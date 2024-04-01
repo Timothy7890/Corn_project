a50afa791ce0d1851fd9ea8ea0575e86b2066e64
timo <1434070486@qq.com>
2024-04-01 18:01:01 +0800

    在代码中添加了更多注释~

--- Run/Generate_midline.py
+++ Run/Generate_midline.py
@@ -1,5 +1,12 @@
 from Midline_extraction import extract_midline
 
+"""
+此代码为一键执行代码，可以直接生成中轴线；
+在运行代码前需要确保原点云已经经过旋转对齐了Box;
+输入输出的点云格式均为x y z R G B Nx Ny Nz.
+"""
+
+
 if __name__ == "__main__":
     # 输入点云文件的路径
     input_txt = "/home/timo/音乐/DELETE/预处理/01/Corn_01.txt"
